fb67976d95e2945b93f46fd50cc466cc2cd451b9
Drew Harvey <drew@vaadin.com>
2026-08-06 15:36:48 -0700

    steps 1, 2, 3
    Basics of creating components, adding to layouts, and using click listeners

--- src/main/java/com/example/ui/HomeView.java
+++ src/main/java/com/example/ui/HomeView.java
@@ -1,6 +1,11 @@
 package com.example.ui;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -13,5 +18,24 @@ import com.vaadin.flow.router.Route;
 class HomeView extends VerticalLayout {
 
     HomeView() {
+        var name = new TextField("Name");
+        add(name);
+
+        var dob = new DatePicker("Date of birth");
+        add(dob);
+
+        var save = new Button("Save");
+        save.addClickListener(e -> {
+            Notification.show("Details saved!");
+        });
+
+        var discard = new Button("Discard");
+        discard.addClickListener(e -> {
+            Notification.show("Details discarded");
+        });
+
+        var buttonsLayout = new HorizontalLayout();
+        buttonsLayout.add(save, discard);
+        add(buttonsLayout);
     }
 }
