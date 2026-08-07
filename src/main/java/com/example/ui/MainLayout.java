9b83ed982ba0149dc6eab380615f7246f8a327ac
Drew Harvey <drew@vaadin.com>
2026-08-06 18:36:51 -0700

    steps 10
    Created UI shell (MainLayout) by extending layout, this includes app logo, name and navigation

---
+++ src/main/java/com/example/ui/MainLayout.java
@@ -0,0 +1,28 @@
+package com.example.ui;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.Layout;
+
+@Layout
+public class MainLayout extends AppLayout {
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+
+        var logo = VaadinIcon.VAADIN_H.create();
+        var appName = new Span("Polaris CRM");
+        var header = new HorizontalLayout(logo, appName);
+        header.setPadding(true);
+
+        var nav = new SideNav();
+        nav.addItem(new SideNavItem("Home", HomeView.class, VaadinIcon.HOME.create()));
+        nav.addItem(new SideNavItem("Customers", CustomerListView.class, VaadinIcon.USER.create()));
+
+        addToDrawer(header, nav);
+    }
+}
